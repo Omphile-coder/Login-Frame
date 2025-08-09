9dbaf6a87444086f7b7d74225ba3309ac6dc2dda
Omphile-coder <lucasomphile23@gmail.com>
2025-08-09 18:24:15 +0200

    Add files via upload

---
+++ LoginFrameApp.java
@@ -0,0 +1,18 @@
+
+package loginframeapp;
+
+import za.tut.ac.ui.LoginFrame;
+
+
+public class LoginFrameApp {
+
+
+    public static void main(String[] args) {
+
+        //instantiation
+        new LoginFrame();
+
+
+    }
+    
+}
